01f784a1017170021a70a1a8e34f255ce8a2fa3d
ShadowOfShadow <limcw3@gmail.com>
2021-08-03 21:11:50 +0900

    modify BOJ11021

--- BOJ11021/src/BOJ11021.java
+++ BOJ11021/src/BOJ11021.java
@@ -1,14 +1,11 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class BOJ11021 {
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int T = Integer.parseInt(st.nextToken());
