e512a96e564d9f56f45ddcf2f54fa4439713d708
ShadowOfShadow <limcw3@gmail.com>
2021-08-03 21:09:17 +0900

    solve BOJ11021

---
+++ BOJ11021/src/BOJ11021.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ11021 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
+		
+		int[] plusArray = new int[T];
+		
+		for(int i = 0; i < T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			plusArray[i] = A+B;
+		}
+		
+		for(int i = 0; i < T; i++) {
+			System.out.print("Case #"+(i+1)+": "+ plusArray[i] +"\n");
+		}
+		
+	}
+
+}
