05d3e5b9544b014640fc474202b5f5ddca7d9bd3
ShadowOfShadow <limcw3@gmail.com>
2021-08-25 23:42:52 +0900

    feat: modify BOJ10952

--- BOJ10952/src/BOJV110952.java
+++ BOJ10952/src/BOJV110952.java
@@ -3,20 +3,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ10952 {
+public class BOJV110952 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 				
+		StringBuilder sb = new StringBuilder();
+		
 		while(true) {
+			
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			
 			int A = Integer.parseInt(st.nextToken());
 			int B = Integer.parseInt(st.nextToken());
 			
-			if(0==A && 0==B) return;
+			if(0==A && 0==B) {
+				break;
+			}
 						
-			System.out.println(A+B);
+			sb.append(A+B).append('\n');
 		}
+		System.out.println(sb);
 	}
 }
---
+++ BOJ10952/src/BOJV210952.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJV210952 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while(true) {
+			String str = br.readLine();
+			
+			int A = str.charAt(0) - 48;
+			int B = str.charAt(0) - 48;
+			
+			if(A==0 && B==0) {
+				break;
+			}
+			
+			sb.append(A+B).append('\n');
+		}
+		System.out.println(sb);
+	}
+
+}
