99e71fb89625b541f0154a7038f00cf26b08e261
ShadowOfShadow <limcw3@gmail.com>
2021-08-27 01:40:20 +0900

    solve BOJ10951

---
+++ BOJ10951/src/BOJ10951.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ10951 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringBuilder sb = new StringBuilder();
+		
+		String str;
+		
+		while((str = br.readLine()) != null) {			
+			
+			int A = str.charAt(0) - 48;
+			int B = str.charAt(2) - 48;
+						
+			sb.append(A+B).append('\n');
+		}
+		System.out.println(sb);
+	}
+
+}
