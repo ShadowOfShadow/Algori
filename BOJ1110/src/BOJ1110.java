c3faf7ac80d60efce57930b7ad0195b4331a724f
ShadowOfShadow <limcw3@gmail.com>
2021-08-28 16:18:57 +0900

    solve BOJ1110

---
+++ BOJ1110/src/BOJ1110.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1110 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		if(N <10)
+			N *= 10;	
+		
+		int temp1;
+		int temp2;
+		int temp3;
+		String result;
+		int i = 0;
+		
+		int O = N;
+		while(true) {
+			temp1 = O % 10; // 첫번째 자리
+			temp2 = O / 10; // 두번째 자리
+			temp3 = temp1+temp2;
+			if(temp3 >= 10) {
+				temp3 = temp3 % 10;				
+			}
+			result = Integer.toString(temp1) + Integer.toString(temp3);
+			i++;
+			if(Integer.valueOf(result) == N) {
+				System.out.println(i);
+				break;
+			}
+			O = Integer.valueOf(result);
+		}
+	}
+}
