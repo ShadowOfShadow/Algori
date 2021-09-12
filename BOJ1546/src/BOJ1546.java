b13e3e007f6317ec9d02f296f20b40c7d9d8d648
ShadowOfShadow <limcw3@gmail.com>
2021-09-12 23:31:04 +0900

    solve BOJ1546

---
+++ BOJ1546/src/BOJ1546.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1546 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int A = Integer.parseInt(st.nextToken());
+		
+		float[] subject = new float[A];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		int i = 0;
+		float max = 0;
+		float sum = 0;
+		int result = 0;
+		
+		while(st.hasMoreTokens()) {
+			subject[i] = Integer.parseInt(st.nextToken());
+			if(subject[i] > max) {
+				max = subject[i];
+			}
+			i++;
+		}
+		
+		
+		for(int j = 0 ; j < subject.length ; j++) {
+			subject[j] = (subject[j]/max)*100;
+			sum += subject[j];
+		}
+		
+		System.out.println(sum/subject.length);
+		
+	}
+}
