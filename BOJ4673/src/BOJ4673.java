26ea1c4c683f3ca6441af2ad055c2dd1e1a2420e
ShadowOfShadow <limcw3@gmail.com>
2021-09-16 05:32:41 +0900

    solve BOJ4673

---
+++ BOJ4673/src/BOJ4673.java
@@ -0,0 +1,39 @@
+
+public class BOJ4673 {
+
+	public static void main(String[] args) {
+		
+		boolean[] check = new boolean[10001];
+		
+		for(int i = 1 ; i < 10001; i++) {
+			int n = selfNum(i);
+			
+			if(n < 10001) {
+				check[n] = true;
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 1 ; i < 10001 ; i++) {
+			if(!check[i]) {
+				sb.append(i).append('\n');
+			}
+		}
+		
+		System.out.println(sb);
+		
+	}
+
+	private static int selfNum(int number) {
+		int sum = number;
+		
+		while(number != 0) {
+			sum = sum + (number % 10);
+			number = number/10;
+		}
+		
+		return sum;
+	}
+
+}
