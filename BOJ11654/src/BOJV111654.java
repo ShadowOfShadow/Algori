0ef11a455827dd73646597bf23570986d5035ab9
ShadowOfShadow <limcw3@gmail.com>
2021-09-18 01:19:40 +0900

    solve BOJ11654

---
+++ BOJ11654/src/BOJV111654.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJV111654 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testCase = br.readLine().charAt(0);
+		
+		System.out.println(testCase);
+	
+	}
+
+}
---
+++ BOJ11654/src/BOJV211654.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class BOJV211654 {
+
+	public static void main(String[] args) throws IOException {
+		
+		int test = System.in.read();
+		System.out.print(test);
+		
+	}
+
+}
