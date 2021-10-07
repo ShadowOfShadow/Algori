a49c3f51c979631d804eac84b2708616ab43371e
ShadowOfShadow <limcw3@gmail.com>
2021-10-07 23:55:12 +0900

    solve BOJ1193

--- BOJ1193/src/BOJ1193.java
+++ BOJ1193/src/BOJ1193.java
@@ -9,6 +9,32 @@ public class BOJ1193 {
 		
 		int N = Integer.parseInt(br.readLine());
 		
+		int cross_count = 1, prev_count_sum = 0;
+		
+		while(true) {
+			// 직진 대각선 누적합 + 해당 대각선 개수 이용한 범위 판별
+			if(N <= prev_count_sum + cross_count) {
+				// 대각선의 개수가 홀수라면
+				if(cross_count %2 == 1) {
+					// 분모가 큰 수부터 시작
+					// 분모는 대각선 개수 - (N번째 - 직전 대각선까지의 누적합 - 1)
+					// 분자는 N 번째 - 직전 대각선까지의 누적합
+					System.out.print((cross_count - (N - prev_count_sum - 1)) + "/" + (N - prev_count_sum));
+					break;
+				}
+				// 대각선의 개수가 짝수라면
+				else {
+					System.out.print((N - prev_count_sum) + "/" + (cross_count - (N - prev_count_sum - 1)));
+					break;
+				}
+			}
+			else {
+				prev_count_sum += cross_count;
+				cross_count++;
+			}
+			
+		}
+		
 	}
 
 }
